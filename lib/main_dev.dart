72337a9efe583101632d6c37645aa7552d71c20d
aprxty3 <ajiprxty3@gmail.com>
2022-11-10 02:45:45 +0700

    setting main env

---
+++ lib/main_dev.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'core/env.dart';
+
+void main() => Development();
+
+class Development extends Env {
+  @override
+  final String appName = 'Name Your Project';
+  @override
+  final String articleUrl = '';
+  @override
+  final String baseUrl = ''; //dev
+  @override
+  final String tnc = '';
+  @override
+  final String socketUrl = ''; // socket.io has connected
+  @override
+  final String websocket =
+      ''; // socket.io reject connecting so websocket handle it
+
+  @override
+  final Color primarySwatch = Colors.teal;
+  @override
+  EnvType environmentType = EnvType.development;
+
+  @override
+  final String dbName = 'dev-docApps.db';
+}
---
+++ lib/main_prod.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'core/env.dart';
+
+void main() => Production();
+
+class Production extends Env {
+  @override
+  final String appName = 'Name Your Project';
+  @override
+  final String articleUrl = '';
+  @override
+  final String baseUrl = ''; //dev
+  @override
+  final String tnc = '';
+  @override
+  final Color primarySwatch = Colors.teal;
+  @override
+  EnvType environmentType = EnvType.production;
+
+  @override
+  final String dbName = '
