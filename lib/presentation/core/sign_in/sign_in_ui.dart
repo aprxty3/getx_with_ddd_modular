d064256b0d9e8fbd4d8f43ad9e235a3f19a5603f
aprxty3 <ajiprxty3@gmail.com>
2022-11-14 19:59:47 +0700

    update ui

--- lib/presentation/core/sign_in/sign_in_ui.dart
+++ lib/presentation/core/sign_in/sign_in_ui.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_with_ddd_modular/presentation/core/sign_in/sign_in_logic.dart';
 
-import 'sign_in_logic.dart';
 
 class SignInUi extends StatelessWidget {
   static const String namePath = '/sign-in';
