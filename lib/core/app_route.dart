5499c22c8465fc0e0f8cc0f86dc9360825da896b
aprxty3 <ajiprxty3@gmail.com>
2022-11-14 21:04:51 +0700

    update env

--- lib/core/app_route.dart
+++ lib/core/app_route.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
 
-import '../feature/core/reset_password/reset_password_binding.dart';
-import '../feature/core/reset_password/reset_password_ui.dart';
-import '../feature/core/sign_in/sign_in_binding.dart';
-import '../feature/core/sign_in/sign_in_ui.dart';
-import '../feature/core/splash_screen/splash_screen_binding.dart';
-import '../feature/core/splash_screen/splash_screen_ui.dart';
+import '../feature/core/presentation/reset_password/reset_password_binding.dart';
+import '../feature/core/presentation/reset_password/reset_password_ui.dart';
+import '../feature/core/presentation/sign_in/sign_in_binding.dart';
+import '../feature/core/presentation/sign_in/sign_in_ui.dart';
+import '../feature/core/presentation/splash_screen/splash_screen_binding.dart';
+import '../feature/core/presentation/splash_screen/splash_screen_ui.dart';
 
 class AppRouter {
   static const initial = SplashScreenUi.namePath;
